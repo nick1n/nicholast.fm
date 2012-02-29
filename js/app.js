b9a2abaf35c97c2f5aa7ec4f76fab1db1c5cf4e5
Nicholas Ness <nnick1n@gmail.com>
2012-02-28 21:21:45 -0600

    removed debug

--- js/app.js
+++ js/app.js
@@ -44,14 +44,11 @@ function getTracks(user) {
 	month = 0;
 	$("#progressBar").width("0%");
 	//document.getElementById("progressPercent").innerHTML = "0%";
-	console.log(user);
 	
 	document.getElementById("arDisplay").style.display = "none";
 	document.getElementById("trackInfo").style.display = "none";
 	$("#progressBack").show();
 	
-	console.log($("#progressBack"));
-	console.log(lastfm.user.getRecentTracks);
 	try {
 		lastfm.user.getRecentTracks({user: username, limit: 1}, {success: getTimeZone, error: failFunction});
 	} catch (e) {}
@@ -65,7 +62,7 @@ function getTimeZone(data) {
 	//if (data.recenttracks.track[1] == undefined) {
 	//    offset = -(new Date().getTimezoneOffset()*60*1000) - (new Date(new Date(data.recenttracks.track.date.uts*1000).setSeconds(0)).getTime()-new Date(data.recenttracks.track.date['#text']).getTime());
 	//}
-	console.log('getTimeZone');
+
 	// Find the from and to dates
 	// need to fix this only works when you are in the same timezone as the scrobbling was
 	var date = new Date();
