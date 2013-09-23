6ef664f41bdc82b6491ef727cc0d87047494c510
Nick <nnick1n@gmail.com>
2013-09-23 17:19:57 -0500

    Minor modifications to the Monthly module
    Related to Issue #50

--- js/monthly.js
+++ js/monthly.js
@@ -40,29 +40,33 @@ function Monthly(year, month) {
 		//setMonth(month);
 
 
-	// these numbers are in seconds, javascript uses milliseconds
-	//var beginnings = [1108296002, 1108900801, 1109505601, 1110110401, 1110715201, 1111320001, 1111924801, 1112529601, 1113134400, 1113739202, 1114344001, 1114965332, 1115553600, 1116158400, 1116759602, 1117364401, 1117969201, 1118577601, 1119182401, 1119787202, 1120392002, 1120996803, 1121601602, 1122206401, 1122811201, 1123416004, 1124020801, 1124625601, 1125230401, 1125835201, 1126440001, 1127044802, 1127649601, 1128254402, 1128859201, 1129464001, 1130068802, 1130673602, 1131278402, 1131883201, 1132488002, 1133092801, 1133697601, 1134302403, 1134907202, 1135512002, 1136116801, 1136721601, 1137326402, 1137931202, 1138536002, 1139140801, 1139745601, 1140350401, 1140955203, 1141560002, 1142164802, 1142769602, 1143374402, 1143979202, 1144584000],
-
-	// anything before this should probably not be trusted (might not be completely correct)
+	// TODO: should probably make some of these "constants"
+	// anything before this date should probably not be trusted (might not be completely correct)
 	var weeklyBegins = 1144584000,
 		week = 604800,
 		oneSecond = 1000,
+		text = '#text',
+		attr = '@attr',
+
 		firstWeek,
 		weeklyFroms = [],
 		weeklyFromsLength,
+
 		//year,
 		//month,
 		date,
 		fromDate,
 		toDate,
+
+		now = Date.now() / oneSecond,
 		requests = 0,
-		i;
+		i = 0;
 
 
 	// Find the from and to dates
 	// need to fix this only works when you are in the same timezone as the scrobbling was
 	date = new Date(year, month, 1);
-	fromDate = date / oneSecond - 1;
+	fromDate = date / oneSecond;
 	date.setMonth(month + 1);
 	toDate = date / oneSecond;
 
@@ -70,7 +74,7 @@ function Monthly(year, month) {
 	// first week from date
 	firstWeek = weeklyBegins + Math.ceil((fromDate - weeklyBegins) / week) * week;
 
-	while (firstWeek + week <= Date.now() / oneSecond && firstWeek + week <= toDate) {
+	for (; firstWeek + week <= now && firstWeek + week <= toDate; firstWeek += week) {
 
 		// if this week falls before correct last.fm weekly charts, skip it
 		if (firstWeek < weeklyBegins) {
@@ -78,40 +82,31 @@ function Monthly(year, month) {
 		}
 
 		weeklyFroms.push(firstWeek);
-		firstWeek += week;
 	}
 
 	weeklyFromsLength = weeklyFroms.length;
 
-	for (i = 0; i < weeklyFromsLength; ++i) {
+	for (; i < weeklyFromsLength; ++i) {
 		// get weekly tracks:
 		getWeeklyTrackChart(weeklyFroms[i]);
 	}
 
-
-	// fixing utc times for last.fm, converting them from being in milliseconds to seconds
-	//fromDate /= oneSecond;
-	//toDate /= oneSecond;
-
-
+	// get the in between weekly tracks for a month
 	if (weeklyFromsLength > 0) {
 
+		// beginning of the month
 		if (weeklyFroms[0] - fromDate > 0) {
-
 			getRecentTracks(1, fromDate, weeklyFroms[0]);
-
 		}
 
+		// ending of the month
 		if (toDate - weeklyFroms[weeklyFromsLength - 1] - week > 0) {
-
 			getRecentTracks(1, weeklyFroms[weeklyFromsLength - 1] + week, toDate);
-
 		}
 
+	// or if there are no weekly tracks to get, just get all recent tracks for the month then
 	} else {
-
 		getRecentTracks(1, fromDate, toDate);
-
 	}
 
 
@@ -119,35 +114,71 @@ function Monthly(year, month) {
 	function getRecentTracks(page, from, to) {
 		++requests;
 
+		// cache recent tracks only when it falls before now
 		LastFM('user.getRecentTracks', {
 			user: username,
 			limit: '200',
 			page: page,
-			from: from,
+			from: from - 1, // subtract one second from all getRecentTracks requests, because I believe its more accurate, since its exclusive.
 			to: to
-		}).done(gotRecentTracks(page, from, to));
+		}, now > to).done(gotRecentTracks(page, from, to));
 	}
 
 	function gotRecentTracks(page, from, to) {
 		return function(data) {
-			var totalPages,
-				i;
+			var tracks,
+				index = 0;
 
 			console.log(page, from, to, data);
 
 			// if '@attr' is missing means no data was returned for this request
-			if (!data.recenttracks['@attr']) {
+			if (!data.recenttracks[attr]) {
 				finished();
 				return;
 			}
 
+			tracks = data.recenttracks.track;
+
 			// if its the first page and there are more to get, get them all
-			totalPages = +data.recenttracks['@attr'].totalPages;
-			for (++page; page < totalPages && page == 1; ++page) {
-				getRecentTracks(page, from, to);
+			if (page == 1) {
+				for (++page; page <= data.recenttracks[attr].totalPages; ++page) {
+					getRecentTracks(page, from, to);
+				}
+
+				// check for extra now playing track even when its tracks from months or years ago
+				// and remove it from the array
+				if (tracks[0] && tracks[0][attr]) {
+					tracks.shift();
+				}
 			}
 
-			// TODO: go through tracks
+			for (; index < tracks.length; ++index) {
+				// TODO: do something with tracks[index]
+				var artist = tracks[index].artist[text];
+				var album = tracks[index].album[text];
+				var track = tracks[index].name;
+
+				/*
+				User.year(2013)
+					.month(8)
+					.artist(artist)
+					.album(album)
+					.track(track)
+					.play(1);
+				
+				// the Storage module should take care of this:
+				Names.add(artist);
+				Names.add(album);
+				Names.add(track);
+
+				var track_id = Track.add(artist, album, track);
+
+				User.add(track_id).toYear(year).andMonth(month).forUser(username);
+
+				Storage.add(artist, album, track, 1);
+
+				*/
+			}
 
 			finished();
 		};
@@ -157,29 +188,39 @@ function Monthly(year, month) {
 	function getWeeklyTrackChart(from) {
 		++requests;
 
-		LastFM('user.getweeklytrackchart', {
+		// always cache weekly tracks, because it should never change
+		LastFM('user.getWeeklyTrackChart', {
 			user: username,
 			from: from
-		}).done(gotWeeklyTrackChart(from));
+		}, true).done(gotWeeklyTrackChart(from));
 	}
 
 	function gotWeeklyTrackChart(from) {
 		return function(data) {
+			var tracks,
+				index = 0;
+
 			console.log(from, data);
 
 			// if '@attr' is missing means no data was returned for this request
-			if (!data.weeklytrackchart['@attr']) {
+			if (!data.weeklytrackchart[attr]) {
 				finished();
 				return;
 			}
 
-			// I believe the weekly track chart get cut off at 500 unique tracks,
+			tracks = data.weeklytrackchart.track;
+
+			// I'm pretty sure the weekly track chart gets cut off at 500 unique tracks,
 			// If so, redo this week by getting the recent tracks for it.
-			if (data.weeklytrackchart.track.length == 500) {
+			if (tracks.length == 500) {
 				getRecentTracks(1, from, from + week);
+				finished();
+				return;
 			}
 
-			// TODO: go through tracks
+			for (; index < tracks.length; ++index) {
+				// TODO: do something with tracks[index]
+			}
 
 			finished();
 		};
@@ -189,6 +230,11 @@ function Monthly(year, month) {
 	function finished() {
 		if (!--requests) {
 			console.log('finished');
+
+			// TODO: do something, maybe save it all back to localStorage or something...
+			/*
+			Storage.saveAll();
+			*/
 		}
 	}
 
