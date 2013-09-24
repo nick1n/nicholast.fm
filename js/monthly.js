cf2f85d43a5fde0b544a4769b932745518939521
Nick <nnick1n@gmail.com>
2013-09-24 02:09:07 -0500

    Well, can't use weekly track charts...
    ...because it doesn't include album information for tracks, so much for
    faster/less api calls.
    Anyway, recent tracks might be the way to go, because they have added an
    extended option for including Artist images and if the user loves the
    track.

--- js/monthly.js
+++ js/monthly.js
@@ -41,16 +41,9 @@ function Monthly(year, month) {
 
 
 	// TODO: should probably make some of these "constants"
-	// anything before this date should probably not be trusted (might not be completely correct)
-	var weeklyBegins = 1144584000,
-		week = 604800,
-		oneSecond = 1000,
+	var oneSecond = 1000,
 		text = '#text',
 		attr = '@attr',
-
-		firstWeek,
-		weeklyFroms = [],
-		weeklyFromsLength,
 
 		//year,
 		//month,
@@ -70,44 +63,7 @@ function Monthly(year, month) {
 	date.setMonth(month + 1);
 	toDate = date / oneSecond;
 
-
-	// first week from date
-	firstWeek = weeklyBegins + Math.ceil((fromDate - weeklyBegins) / week) * week;
-
-	for (; firstWeek + week <= now && firstWeek + week <= toDate; firstWeek += week) {
-
-		// if this week falls before correct last.fm weekly charts, skip it
-		if (firstWeek < weeklyBegins) {
-			continue;
-		}
-
-		weeklyFroms.push(firstWeek);
-	}
-
-	weeklyFromsLength = weeklyFroms.length;
-
-	for (; i < weeklyFromsLength; ++i) {
-		// get weekly tracks:
-		getWeeklyTrackChart(weeklyFroms[i]);
-	}
-
-	// get the in between weekly tracks for a month
-	if (weeklyFromsLength > 0) {
-
-		// beginning of the month
-		if (weeklyFroms[0] - fromDate > 0) {
-			getRecentTracks(1, fromDate, weeklyFroms[0]);
-		}
-
-		// ending of the month
-		if (toDate - weeklyFroms[weeklyFromsLength - 1] - week > 0) {
-			getRecentTracks(1, weeklyFroms[weeklyFromsLength - 1] + week, toDate);
-		}
-
-	// or if there are no weekly tracks to get, just get all recent tracks for the month then
-	} else {
-		getRecentTracks(1, fromDate, toDate);
-	}
+	getRecentTracks(1, fromDate, toDate);
 
 
 
@@ -119,6 +75,7 @@ function Monthly(year, month) {
 			user: username,
 			limit: '200',
 			page: page,
+			extended: 1,
 			from: from - 1, // subtract one second from all getRecentTracks requests, because I believe its more accurate, since its exclusive.
 			to: to
 		}, now > to).done(gotRecentTracks(page, from, to));
@@ -153,11 +110,13 @@ function Monthly(year, month) {
 			}
 
 			for (; index < tracks.length; ++index) {
-				// TODO: do something with tracks[index]
-				var artist = tracks[index].artist[text];
+
+				// artist.name when its extended data and artist[text] when not
+				var artist = tracks[index].artist.name || tracks[index].artist[text];
 				var album = tracks[index].album[text];
 				var track = tracks[index].name;
 
+				// TODO: do something with tracks[index]
 				/*
 				User.year(2013)
 					.month(8)
@@ -165,7 +124,7 @@ function Monthly(year, month) {
 					.album(album)
 					.track(track)
 					.play(1);
-				
+
 				// the Storage module should take care of this:
 				Names.add(artist);
 				Names.add(album);
@@ -183,49 +142,6 @@ function Monthly(year, month) {
 			finished();
 		};
 	}
-
-
-	function getWeeklyTrackChart(from) {
-		++requests;
-
-		// always cache weekly tracks, because it should never change
-		LastFM('user.getWeeklyTrackChart', {
-			user: username,
-			from: from
-		}, true).done(gotWeeklyTrackChart(from));
-	}
-
-	function gotWeeklyTrackChart(from) {
-		return function(data) {
-			var tracks,
-				index = 0;
-
-			console.log(from, data);
-
-			// if '@attr' is missing means no data was returned for this request
-			if (!data.weeklytrackchart[attr]) {
-				finished();
-				return;
-			}
-
-			tracks = data.weeklytrackchart.track;
-
-			// I'm pretty sure the weekly track chart gets cut off at 500 unique tracks,
-			// If so, redo this week by getting the recent tracks for it.
-			if (tracks.length == 500) {
-				getRecentTracks(1, from, from + week);
-				finished();
-				return;
-			}
-
-			for (; index < tracks.length; ++index) {
-				// TODO: do something with tracks[index]
-			}
-
-			finished();
-		};
-	}
-
 
 	function finished() {
 		if (!--requests) {
