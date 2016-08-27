ddd9baa5723ef01bc41f54e553d1b9401a65c5b5
Nick <nnick1n@gmail.com>
2016-08-27 01:59:25 -0500

    Renamed css files to scss and added sass support by modifing reace-scripts' webpack configs like: https://github.com/facebookincubator/create-react-app/pull/115/files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './index.scss';
+
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <App />,
