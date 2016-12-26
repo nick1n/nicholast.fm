f59563fbe836bfc8f7ec486f5c8c39d8b3119eba
Nick <nnick1n@gmail.com>
2016-12-25 20:55:18 -0600

    Updated everything

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { render } from 'react-dom';
+import { Router, Route, /*Link,*/ browserHistory } from 'react-router';
 
 import './index.scss';
 
 import App from './App';
 
-ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App} />
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+{/*
+      <Route path="about" component={About}/>
+      <Route path="users" component={Users}>
+        <Route path="/user/:userId" component={User}/>
+      </Route>
+      <Route path="*" component={NoMatch}/>
+*/}
+    </Route>
   </Router>
 ), document.getElementById('root'));
