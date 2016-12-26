51267a3e6fce252aa55bb81d9ddbff2fd760ccfa
Nick <nnick1n@gmail.com>
2016-12-25 20:56:36 -0600

    Removed react templated app logo section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
-
 import './App.scss';
 
 import Header from './Header';
@@ -12,10 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
