ff751eea736095b8af1c2efce4fa74e4497076da
Nick <nnick1n@gmail.com>
2017-04-16 01:26:35 -0500

    Removed React Router, keeping it simple for now

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route/*, Link*/ } from 'react-router-dom'
 
 import './index.scss';
 
 import App from './App';
 
-render((
-  <BrowserRouter>
-    <Route path="/" component={App}>
-{/*
-      <Route path="/about" component={About}/>
-      <Route path="/users" component={Users}>
-        <Route path="/user/:userId" component={User}/>
-      </Route>
-      <Route path="*" component={NoMatch}/>
-*/}
-    </Route>
-  </BrowserRouter>
-), document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
