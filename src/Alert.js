7defa419c8a48d384aeee3986a8fbe3ba2776f4d
Nick <nnick1n@gmail.com>
2017-04-17 02:23:38 -0500

    Some work on the Alert component

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
+import './Alert.scss';
+
 class Alert extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+
+    setTimeout(() => {
+      this.setState({
+        open: true
+      });
+    }, 100);
+  }
+
+  close = () => {
+    this.setState({
+      open: false
+    });
+  };
+
   render() {
     return (
-      <div className="alert alert-success">
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+      <div className={'alert alert-success ' + this.state.open}>
+        <button type="button" className="close" aria-label="Close" onClick={this.close}>
+          <i className="fa fa-times" aria-hidden="true"></i>
         </button>
+        <i className="fa fa-spin fa-circle-o-notch" aria-hidden="true"></i>
         <strong>Welcome</strong> to the new nicholast.fm!
       </div>
     );
