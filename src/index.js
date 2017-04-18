9f11146ed62469ad0a28bc1ee533fa3477fd1eeb
Nick <nnick1n@gmail.com>
2017-04-18 01:24:12 -0500

    Updated some create-react-app files and updated React to 15.5.4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import './index.scss';
 
 import App from './App';
 
-render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
